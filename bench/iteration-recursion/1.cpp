199670cbf6687993c5d08b2ef3c63f9f106202bc
Florentin Bekier <florentin@bekier.fr>
2020-03-16 14:59:03 -0500

    Update C++ benchmark 3

--- bench/iteration-recursion/1.cpp
+++ bench/iteration-recursion/1.cpp
@@ -5,14 +5,17 @@ using namespace std;
 int main(int argc, char const *argv[])
 {
 	int N = 1000;
+	if (argc == 2)
+	{
+		N = atoi(argv[1]);
+	}
+
 	int n = 0;
 
 	for (int i = 0; i < N; i++)
 	{
 		n++;
 	}
-	
-	cout << "Done!";
 
 	return 0;
 }
--- bench/iteration-recursion/2.cpp
+++ bench/iteration-recursion/2.cpp
@@ -2,24 +2,28 @@
 
 using namespace std;
 
+// Recursion function
 int rec(int n, int N)
 {
 	if (N <= 0)
 	{
 		return n;
 	}
-	
+
 	return rec(n + 1, N - 1);
 }
 
 int main(int argc, char const *argv[])
 {
 	int N = 1000;
+	if (argc == 2)
+	{
+		N = atoi(argv[1]);
+	}
+
 	int n = 0;
 
 	rec(n, N);
-
-	cout << "Done!";
 
 	return 0;
 }
