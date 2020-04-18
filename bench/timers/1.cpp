adfb3f6afa350d52409ea75890892a9a4dd490c9
Florentin Bekier <florentin@bekier.fr>
2020-04-18 14:44:09 -0500

    Add paramater duration in benchmark 11

--- bench/timers/1.cpp
+++ bench/timers/1.cpp
@@ -6,11 +6,17 @@ using namespace std;
 
 int main(int argc, char const *argv[])
 {
+    int n = 1000000000; // 1 second
+	if (argc == 2)
+	{
+		n = atoi(argv[1]);
+	}
+
     auto start = chrono::steady_clock::now();
     auto end = chrono::steady_clock::now();
     long long int counter = 0;
     
-    while (chrono::duration_cast<chrono::nanoseconds>(end - start).count() < 1000000000) {
+    while (chrono::duration_cast<chrono::nanoseconds>(end - start).count() < n) {
         end = chrono::steady_clock::now();
         counter++;
     }
--- bench/timers/1.go
+++ bench/timers/1.go
@@ -2,14 +2,22 @@ package main
 
 import (
 	"fmt"
+	"os"
+	"strconv"
 	"time"
 )
 
 func main() {
+	var b = 1_000_000_000 // 1 second
+	var args = os.Args[1:]
+	if len(args) >= 1 {
+		n, _ = strconv.Atoi(args[0])
+	}
+
 	var start = time.Now()
 	var end = time.Now()
 	var counter = 0
-	for end.Sub(start) < 1000000000 {
+	for end.Sub(start) < n {
 		end = time.Now()
 		counter++
 	}
